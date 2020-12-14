658f12583c98ad9bcc72a725df4574beaa713880
Michael Oppitz <michael@shapediver.com>
2020-12-14 17:20:54 +0100

    implemented first api version

--- packages/viewer.examples.simple/src/index.ts
+++ packages/viewer.examples.simple/src/index.ts
@@ -1,22 +1,32 @@
-import 'reflect-metadata';
-import { RendererType, RenderingEngineManagement } from '@shapediver/viewer.visualization-engine.rendering-engine-management';
-import { Tree } from '@shapediver/viewer.node-tree.tree';
-import { SessionEngine } from '@shapediver/viewer.session-engine.session-engine';
-import { container } from 'tsyringe';
+import "reflect-metadata"
+import * as api from "@shapediver/viewer"
+import { RendererType } from "@shapediver/viewer";
 
 const modelViewUrl = 'https://sdeuc1.eu-central-1.shapediver.com';
-const ticket1 = 'd6f62ac43b39b2899c85de0258e4f395a49617f6c485da65f1450430f8991e1c31231c434b3504254444b4bb81bc7799e26056b92fcd2fd8f8f1500bbdf73867ed2e87862a9a1349bb182bd4d4a764ff4689bfe19a87b07ebff5847565a83db1ab3002ec006a90841bed2a95fa3ae9663655e05febde-78055df2d71f54f8ca8d3815a352e2c8';
+const ticket = 'd6f62ac43b39b2899c85de0258e4f395a49617f6c485da65f1450430f8991e1c31231c434b3504254444b4bb81bc7799e26056b92fcd2fd8f8f1500bbdf73867ed2e87862a9a1349bb182bd4d4a764ff4689bfe19a87b07ebff5847565a83db1ab3002ec006a90841bed2a95fa3ae9663655e05febde-78055df2d71f54f8ca8d3815a352e2c8';
 
 const init = async () => {
-    const sceneTree = new Tree();
-    sceneTree.addNode(await new SessionEngine(ticket1, modelViewUrl).init());
+    await api.createSession(ticket, modelViewUrl, 'mySession');
+    await api.createViewer(RendererType.THREE, <HTMLCanvasElement>document.getElementById('canvas'), 'myViewer')
+}
+init();
 
-    const renderingEngineManagement = <RenderingEngineManagement>container.resolve(RenderingEngineManagement);
-    let engine = renderingEngineManagement.createNewRenderingEngine(RendererType.THREE, 'rendering-engine', "canvas");
-    engine.updateSceneTree(sceneTree.root);
+(<any>window).sceneTree = api.sceneTree;
 
-    (<HTMLCanvasElement>document.getElementById('canvas')).width = window.innerWidth;
-    (<HTMLCanvasElement>document.getElementById('canvas')).height = window.innerHeight;
+(<any>window).changeParameter = async (name: string, value: string) => {
+    const param = api.getSession('mySession').getParameterByName(name);
+    for(let i = 0; i < param.length; i++)
+        param[i].value = value;
+    await api.getSession('mySession').customize();
 }
 
-init();
+(<any>window).addSDTFOutput = async (uri: string) => {
+    const session = api.getSession('mySession');
+    const output = session.createOutput('sdtfFile');
+    output.content = [];
+    output.content.push({
+        format: 'sdtf',
+        href: uri
+    });
+    await session.customize();
+}
