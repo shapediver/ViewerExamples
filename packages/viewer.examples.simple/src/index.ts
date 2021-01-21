8ae651469f4c331d5c34419997417e9d003e8428
Michael Oppitz <michael@shapediver.com>
2021-01-21 13:36:26 +0100

    dependency changes

--- packages/viewer.examples.simple/src/index.ts
+++ packages/viewer.examples.simple/src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
-import { api } from "@shapediver/viewer"
-import { RendererType } from "@shapediver/viewer";
+import { api } from "@shapediver/viewer.api"
+import { RendererType } from "@shapediver/viewer.api";
 
 const modelViewUrl = 'https://sdeuc1.eu-central-1.shapediver.com';
 const ticket = 'd6f62ac43b39b2899c85de0258e4f395a49617f6c485da65f1450430f8991e1c31231c434b3504254444b4bb81bc7799e26056b92fcd2fd8f8f1500bbdf73867ed2e87862a9a1349bb182bd4d4a764ff4689bfe19a87b07ebff5847565a83db1ab3002ec006a90841bed2a95fa3ae9663655e05febde-78055df2d71f54f8ca8d3815a352e2c8';
